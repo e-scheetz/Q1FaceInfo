52ccc2a964b815c047d6962c5be10ad2f774d7e2
e-scheetz <e.dietrich.scheetz@gmail.com>
2018-09-17 21:56:19 -0600

    transition to photo compression instead of external library

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const tinify = require("./node_modules/tinify");
+  tinify.key = "bQlO3JqRybvDD8Qrv13tIFFQMjef99ur";
+
+  // tinify.fromFile("unoptimized.png").toFile("optimized.png");
+
   var elems = document.querySelectorAll('.fixed-action-btn');
   var instances = M.FloatingActionButton.init(elems, {
     toolbarEnabled: true
@@ -6,10 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems2 = document.querySelectorAll('.tooltipped');
   var instances2 = M.Tooltip.init(elems2);
   axios.get('https://photos.app.goo.gl/PWUbgDcymgCeiQZ97')
-    .then(function (response){
+    .then(function(response) {
       console.log(response)
     })
-    .catch(function (error){
+    .catch(function(error) {
       console.log(error)
     })
 });

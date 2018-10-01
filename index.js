e7f48434f4b3c2ab46b30242a308eeb518b41ee2
e-scheetz <e.dietrich.scheetz@gmail.com>
2018-09-30 19:39:23 -0600

    1.02d hotfix -- error fixes

--- index.js
+++ index.js
@@ -480,7 +480,7 @@ function closeModal2() {
 }
 
 function deleteSingleImage() {
-  let variable = document.getElementsByClassName('active')[0].children[0].src
+  let variable = document.getElementsByClassName('active')[1].children[0].src
   for (let i = 0; i < storedPhotos.length; i++) {
     if (variable === storedPhotos[i]) {
       storedPhotos.splice(i, 1)
